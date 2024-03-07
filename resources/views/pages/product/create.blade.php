0753c75ac839078a953af283cfee50c26067a2f7
xrafff <xrafff@xrafffs-MacBook-Air.local>
2024-03-07 11:35:16 +0700

    first commit

---
+++ resources/views/pages/product/create.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app')
+
+@section('title', 'Product Create')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header d-flex justify-content-between">
+                <h1>Add Product</h1>
+                <a href="{{ route('product.index') }}" class="btn btn-primary">Back</a>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Product</h2>
+
+
+
+                <div class="card">
+                    <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-header">
+                            <h4>Input Text</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text"
+                                    class="form-control @error('name')
+                                is-invalid
+                            @enderror"
+                                    name="name">
+                                @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Price</label>
+                                <input type="number"
+                                    class="form-control @error('price')
+                                is-invalid
+                            @enderror"
+                                    name="price">
+                                @error('price')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Stock</label>
+                                <input type="number"
+                                    class="form-control @error('stock')
+                                is-invalid
+                            @enderror"
+                                    name="stock">
+                                @error('stock')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="form-label">Category</label>
+                                <select class="form-control selectric @error('category_id') is-invalid @enderror"
+                                    name="category_id">
+                                    <option value="">-- Select Category --</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                                            {{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label>Photo Product</label>
+                                <div class="col-sm-9">
+                                    <input type="file" class="form-control" name="image"
+                                        @error('image') is-invalid @enderror>
+                                </div>
+                                @error('image')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+@endpush
